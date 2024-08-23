66f180663268f9d01ce5fc1f42e914957daa36db
CptJack <leninchen03@gmail.com>
2024-08-23 16:34:28 +0800

    2023 B

---
+++ 2023/B-Schedule/verify.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import subprocess
+import sys
+
+def execute_bash_command(command):
+    result = subprocess.run("bash -c \""+command+"\"", shell=True, capture_output=True, text=True)
+    if result.returncode == 0:
+        output = result.stdout
+        return output
+
+def compatible(a,b):
+    # print("compatible")
+    a=[i-1 for i in a]
+    b=[i-1 for i in b]
+    # print(a)
+    # print(b)
+    flag=[False,False,False,False]
+    for i in range(len(a)):
+        ind=(a[i])*2+(b[i])
+        # print(ind)
+        flag[ind]=True
+        if flag==[True,True,True,True]:
+            return True
+    # print(flag)
+    return flag==[True,True,True,True]
+
+files=execute_bash_command("ls data/*.in").strip()
+for f in files.split():
+    # f="data/sample-2.in"
+    print(f)
+    out=execute_bash_command("cat "+f+" | ./main").strip()
+    ans=execute_bash_command("cat "+f[:-3]+".ans").strip()
+    # print(ans)
+    if ans=="infinity":
+        # print(ans)
+        # exit()
+        if out!="infinity":
+            print("failed")
+            exit()
+        else:
+            continue
+    out=out.splitlines()
+    ans=ans.splitlines()
+    isolation=int(out[0])
+    isolation_ans=int(ans[0])
+    # print(isolation)
+    # print(isolation_ans)
+    if isolation!=isolation_ans:
+        print("failed")
+        exit()
+    sched=[[0 for col in range(len(out)-1)] for row in range(len(out[1]))]
+    for i in range(len(out[1])):
+        for j in range(len(out)-1):
+            sched[i][j]=int(out[j+1][i])
+    # for i in range(len(out[1])):
+    #     print(sched[i])
+    # sched[1]=[1,1,1,1,1,1]
+    for i in range(len(out[1])):
+        for j in range(i+1,len(out[1])):
+            if not compatible(sched[i],sched[j]):
+                print("failed")
+                exit()
+    # exit()
+    # break
+print("succ")
