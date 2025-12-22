ddbbe7ce93e0a5cc3d517676a62cfb5ac4b2b81b
CptJack <leninchen03@gamil.com>
2025-12-22 18:37:45 +0800

    g

---
+++ 2023/G-TurningRed/main.cpp
@@ -0,0 +1,79 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int l,b;
+vector<char> lights;
+vector<vector<int>> button_control_lights;
+vector<set<int>> light_related_button;
+vector<int> solution;
+
+int main(){
+    cin>>l>>b;
+    //灯和按钮的下标都是1开始
+    lights.resize(l+1);
+    light_related_button.resize(l+1);
+    map<char,char> lmap={{'R',0},{'G',1},{'B',2}};
+    for(int i=1;i<=l;++i){
+        char t;
+        cin>>t;
+        lights[i]=lmap[t];
+    }
+
+    button_control_lights.resize(b+1);
+    solution.resize(b+1);
+    for(int i=1;i<=b;++i)solution[i]=-1;
+    for(int i=1;i<=b;++i){
+        int k;
+        cin>>k;
+        for(int j=0;j<k;++j){
+            int x;
+            cin>>x;
+            light_related_button[x].insert(i);
+            button_control_lights[i].push_back(x);
+        }
+    }
+
+//    auto light_related_button_org=light_related_button;
+    while(true){
+        int i=1;
+        for (;i<=l;++i)
+            if(light_related_button[i].size()==1)
+                break;
+        if(i>l)
+            break;
+        int but=*light_related_button[i].begin();
+        if(solution[but]==-1){
+            int press_times=(3-lights[i])%3;
+            solution[but]=press_times;
+            for(auto li:button_control_lights[but]){
+                light_related_button[li].erase(but);
+                lights[li]=(lights[li]+press_times)%3;
+            }
+
+        }else{
+            cout<<"impossible"<<endl;
+            return 0;
+        }
+    }
+
+    for(int i=1;i<=l;++i)
+        if(lights[i]!=0){
+            if(light_related_button[i].empty()){
+                cout<<"impossible"<<endl;
+                return 0;
+            }
+            int press_times=(3-lights[i])%3;
+            int but=*light_related_button[i].begin();
+            if(solution[but]!=-1)
+                but=*light_related_button[i].rbegin();
+            solution[but]=press_times;
+            lights[i]=0;
+            continue;
+        }
+    int tot=0;
+    for(int i=1;i<=b;++i)
+        if(solution[i]!=-1)
+            tot+=solution[i];
+    cout<<tot<<endl;
+    return 0;
+}
