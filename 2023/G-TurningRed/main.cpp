aca042e4c1f89d13ee0cc9676d19b9b898dc29b6
CptJack <leninchen03@gamil.com>
2025-12-25 17:55:43 +0800

    g one dfs

--- 2023/G-TurningRed/main.cpp
+++ 2023/G-TurningRed/main.cpp
@@ -32,97 +32,38 @@ int main(){
             button_control_lights[i].push_back(x);
         }
     }
-
-    vector<vector<int>> connected_components;
-    int light_cnt=0;
-    vector<int> visited_li(l+1,false);
-    while(light_cnt<l){
-        int i=1;
-        for(;i<=l;i++)if(!visited_li[i])break;
-        if(i>l)break;
-        vector<int> comp;
-        stack<int> li_stack;
-        li_stack.push(i);visited_li[i]=true;
-        while(!li_stack.empty()){
-            auto li=li_stack.top();
-            li_stack.pop();
-            ++light_cnt;
-            comp.push_back(li);
-            for(auto bu :light_related_button[li])
-                for(auto l2:button_control_lights[bu])
-                    if(!visited_li[l2]) {
-                        li_stack.push(l2);
-                        visited_li[l2]=true;
-                    }
-        }
-        connected_components.push_back(comp);
-    }
-//    cout<<"DONE CC"<<endl;
     int ans =0;
 
-    for(auto cc:connected_components){
-        if(cc.size()==1 ){
-            if(light_related_button[cc.front()].empty() && lights[cc.front()]!=0) {
-                cout<<"impossible"<<endl;
-                return 0;
-            }
-            int press_times = (3 - lights[cc.front()]) % 3;
-            ans+=press_times;
-            continue;
-        }
-
-        auto lf=cc.front();
-//        set<int> visited;
-
-        std::function<bool(int)> recur=[&](int ll)->bool{
-            vector<int> visited(l+1,false);
-            stack<int> recur_stack;
-            recur_stack.push(ll);
-//            visited.insert(l);
-            //每个节点进来,先判断是不是所有的路径的解都求出来了
-            //如果求出来了,就判断是否符合按压次数
-            //如果没求出来,求解
-            //无论是否求解了,都从所有路径扩散出去
-            while(!recur_stack.empty()){
-                auto lt=recur_stack.top();
-                recur_stack.pop();
-                if (visited[lt])continue;
-                visited[lt]=true;
-//                if (visited.count(l))continue;
-//                visited.insert(l);
-
-                int press_times = (3 - lights[lt]) % 3;
-                //获取这个灯关联的两个按钮
-                auto related_button = light_related_button[lt];
+    class node{
+    public:
+        int type;//1 light 2 button
+        int index;
+    };
+    vector<int> lvisited(l+1,false);
+    vector<int> bvisited(b+1,false);
+    for(int i=1;i<=l;++i){
+        if(lvisited[l])continue;
+        int min_tot=0x7FFFFFFF;
+        bool found_sol=false;
+        for(int i=0;i<=2;++i)
+            for(int j=0;j<=2;++j){
+            vector<int> connected_componet;
+            std::function<void(node)> recur = [&](node n) {
+                if (n.type == 1) {
+                    if (lvisited[n.index])return;
+                    lvisited[n.index] = true;
+                    connected_componet.push_back(n.index);
+                } else {
+                    if (bvisited[n.index])return;
+                    bvisited[n.index] = true;
+                }
+                auto related_button = light_related_button[li];
                 auto but1 = *related_button.begin();
                 auto but2 = related_button.size() == 2 ? *related_button.rbegin() : -1;
-                //获取两个按钮按下的次数
-                auto p1 = solution[but1];
-                auto p2 = but2 != -1 ? solution[but2] : 0;//如果只有一个按钮,虚拟按钮次数为0
 
-                if (p1 != -1 && p2 != -1) {
-                    if ((p1 + p2) % 3 != press_times)return false;
-                } else {
-                    auto &sol = p2 == -1 ? solution[but2] : solution[but1];
-                    if (p1 == -1)swap(p1, p2);
-                    p2 = press_times - p1;
-                    while (p2 < 0)p2 += 3;
-                    sol = p2;
-                }
-
-                for (auto l2: button_control_lights[but1])
-                    if(!visited[l2])
-                        recur_stack.push(l2);
-//                    if (!recur(l2))return false;
-                if (but2 != -1)
-                    for (auto l2: button_control_lights[but2])
-                        if(!visited[l2])
-                            recur_stack.push(l2);
-//                        if (!recur(l2))return false;
-
-            }
-            return true;
-        };
+            };
+        }
+    }
 
         int min_tot=0x7FFFFFFF;
         bool found_sol=false;
@@ -149,7 +90,6 @@ int main(){
             return 0;
         }
         ans+=min_tot;
-    }
 
     cout<<ans<<endl;
     return 0;
