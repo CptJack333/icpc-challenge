c1feddde1ef1aaedc02b999ded811aace83704f5
CptJack <leninchen03@gamil.com>
2025-12-26 15:45:10 +0800

    g , fucking fast!

--- 2023/G-TurningRed/main.cpp
+++ 2023/G-TurningRed/main.cpp
@@ -5,7 +5,6 @@ int l,b;
 vector<char> lights;
 vector<vector<int>> button_control_lights;
 vector<set<int>> light_related_button;
-vector<int> solution;
 
 int main(){
     cin>>l>>b;
@@ -20,8 +19,6 @@ int main(){
     }
 
     button_control_lights.resize(b+1);
-    solution.resize(b+1);
-    for(int i=1;i<=b;++i)solution[i]=-1;
     for(int i=1;i<=b;++i){
         int k;
         cin>>k;
@@ -41,18 +38,21 @@ int main(){
         node(int t,int i):type(t),index(i){}
     };
 
-    vector<int> lvisited(l+1,false);
-    vector<int> bvisited(b+1,false);
+    vector<int> li_visited(l + 1, false);
+    vector<int> bu_visited(b + 1, false);
+    //把灯和按钮画成连通图，分开各个连通分量处理
     vector<vector<node>> connected_componets;
 
     for(int li=1;li<=l;++li){
-        if(lvisited[li])continue;
+        if(li_visited[li])continue;
 
+        //特殊情况处理, 孤立一盏灯, 且没有按钮控制
         if(light_related_button[li].empty()&& lights[li] != 0){
             cout<<"impossible"<<endl;
             return 0;
         }
 
+        //特殊情况, 孤立的一盏灯,但有按钮可以控制. 这里如果不单独处理, 按照两个按钮不断试找最小的方法, 会导致很慢.
         bool single_light=true;
         for (auto b2:light_related_button[li])
             for(auto l2:button_control_lights[b2])
@@ -67,40 +67,43 @@ int main(){
         }
 
         int min_tot=0x7FFFFFFF;
-        bool found_sol=false;
+        bool found_solution=false;
         vector<node> connected_componet;
 
+        //两个按钮, 各按0-2次, 不断组合去试, 得出按的最少次数
         for(int i=0;i<=2;++i)
         for(int j=0;j<=2;++j){
             connected_componet.clear();
+            vector<int> solution(b+1,-1);//每个按钮分别按多少次
 
-            auto recur=[&](node nn){
+            auto try_push=[&](node nn){
                 stack<node> recur_stack;
                 recur_stack.push(nn);
                 while (!recur_stack.empty()) {
                     auto n =recur_stack.top();
                     recur_stack.pop();
-
+                    //灯和按钮分开类型处理
                     if (n.type == 1) {
 //                        if (light_related_button[n.index].empty() && lights[n.index] != 0) {
 //                            return false;
 //                        }
 
-                        if (lvisited[n.index])continue;
-                        lvisited[n.index] = true;
+                        if (li_visited[n.index])continue;
+                        li_visited[n.index] = true;
                     } else {
-                        if (bvisited[n.index])continue;
-                        bvisited[n.index] = true;
+                        if (bu_visited[n.index])continue;
+                        bu_visited[n.index] = true;
                     }
                     connected_componet.push_back(n);
-
+                    //按钮只需要推栈
                     if (n.type == 2) {
                         for (auto l3: button_control_lights[n.index])
-                            if (!lvisited[l3])
+                            if (!li_visited[l3])
                                 recur_stack.emplace(1, l3);
                         continue;
                     }
 
+                    //灯需要判断按的次数是否可行
                     int press_times = (3 - lights[n.index]) % 3;
                     //获取这个灯关联的两个按钮
                     auto related_button = light_related_button[n.index];
@@ -122,26 +125,25 @@ int main(){
                     }
 
                     for (auto b: light_related_button[n.index])
-                        if (!bvisited[b])
+                        if (!bu_visited[b])
                             recur_stack.push(node(2, b));
 
                 }
                 return true;
             };
 
-            solution=vector<int>(solution.size(),-1);
             auto related_button=light_related_button[li];
             if(related_button.size()>=1)
                 solution[*related_button.begin()]=i;
             if(related_button.size()==2)
                 solution[*related_button.rbegin()]=j;
-            if(recur(node(1,li))){
-                if(!found_sol) {
+            if(try_push(node(1, li))){
+                if(!found_solution) {//第一次找到解决方案, 把当前的连通分量记下来
                     sort(connected_componet.begin(), connected_componet.end(),
                          [] (const node& a, const node& b) { return a.type < b.type || (a.type==b.type&& a.index<b.index); });
                     connected_componets.push_back(connected_componet);
                 }
-                found_sol=true;
+                found_solution=true;
                 int tot=0;
                 for(int k : solution)
                     if(k!=-1)
@@ -150,19 +152,19 @@ int main(){
             }
             for(auto n:connected_componet)
                 if(n.type==1)
-                    lvisited[n.index]=false;
+                    li_visited[n.index]=false;
                 else
-                    bvisited[n.index]=false;
+                    bu_visited[n.index]=false;
         }
-        if(!found_sol){
+        if(!found_solution){
             cout<<"impossible"<<endl;
             return 0;
         }
         for(auto n:connected_componets.back())
             if(n.type==1)
-                lvisited[n.index]=true;
+                li_visited[n.index]=true;
             else
-                bvisited[n.index]=true;
+                bu_visited[n.index]=true;
         connected_componet.clear();
         ans+=min_tot;
     }
