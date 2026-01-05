d2e6fdb3cfdb91e24b79235c3c327bc3af2bd157
CptJack <leninchen03@gamil.com>
2026-01-05 17:30:06 +0800

    25 f wa

--- 2025/F-HerdingCats/main.cpp
+++ 2025/F-HerdingCats/main.cpp
@@ -7,13 +7,17 @@ int main(){
     for(int tc=0;tc<t;++tc){
         cin>>n>>m;
         vector<int> plant_after(m+1,0);
+        vector<vector<set<int>>> pot_should_plant(m+1);
         for(int ri=0;ri<n;++ri){
             cin>>p>>k;
+            set<int> plants;
             for(int rj=0;rj<k;++rj) {
                 int plant_index;
                 cin>>plant_index;
+                plants.insert(plant_index);
                 plant_after[plant_index]=max(plant_after[plant_index],p);
             }
+            pot_should_plant[p].push_back(plants);
         }
         vector<int> after_count(m+1,0);
         for(auto pot_index:plant_after)
@@ -24,9 +28,34 @@ int main(){
                 can_do=false;
                 break;
             }
-        if(can_do)
-            cout<<"yes"<<endl;
-        else
-            cout<<"no"<<endl;
+        if(!can_do) {
+            cout << "no" << endl;
+            continue;
+        }
+        for(int pi=1;pi<=m;++pi){
+            if(pot_should_plant[pi].empty())continue;
+            auto set1=pot_should_plant[pi].front();
+            for(auto ss:pot_should_plant[pi]) {
+                std::set<int> temp_intersection; // 临时变量存储本次交集结果
+                std::set_intersection(
+                        set1.begin(), set1.end(),
+                        ss.begin(), ss.end(),
+                        std::inserter(temp_intersection, temp_intersection.begin())
+                );
+                set1 = std::move(temp_intersection); // 将本次交集结果赋给 set1，用于下次计算
+                if (set1.empty()) { // 提前终止，交集已为空
+                    break;
+                }
+            }
+            if (set1.empty()) {
+                can_do = false;
+                break;
+            }
+        }
+        if(!can_do) {
+            cout << "no" << endl;
+            continue;
+        }
+        cout<<"yes"<<endl;
     }
 }
