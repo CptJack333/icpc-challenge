6a9cfdf7dce292694966d4e374a717d8b685cf94
CptJack <leninchen03@gamil.com>
2026-01-07 07:49:32 +0800

    25 f done!

--- 2025/F-HerdingCats/main.cpp
+++ 2025/F-HerdingCats/main.cpp
@@ -1,123 +1,81 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+set<int> set_intersect(vector<set<int>> sets){
+    auto set1=sets.front();
+    for(auto ss:sets) {
+        std::set<int> temp_intersection; // 临时变量存储本次交集结果
+        std::set_intersection(
+                set1.begin(), set1.end(),
+                ss.begin(), ss.end(),
+                std::inserter(temp_intersection, temp_intersection.begin())
+        );
+        set1 = std::move(temp_intersection); // 将本次交集结果赋给 set1，用于下次计算
+        if (set1.empty()) { // 提前终止，交集已为空
+            break;
+        }
+    }
+    return set1;
+}
+
 int main(){
     int t,n,m,p,k;
     cin>>t;
     for(int tc=0;tc<t;++tc){
-        bool cout_test_case= true;
+        bool cout_test_case= false;
         cin>>n>>m;
         if(cout_test_case)
-            cout<<"test case "<<tc<<endl<<n<<m<<endl;
-        vector<int> plant_after(m+1,0);
+            cout<<"test case "<<tc<<endl<<n<<" "<<m<<endl;
+        vector<int> catnip_lower_bound(m + 1, 0);
+        set<int> key_pots;
         vector<vector<set<int>>> pot_should_plant(m+1);
+//        读入数据，计算猫草lb
         for(int ri=0;ri<n;++ri){
             cin>>p>>k;
+            key_pots.insert(p);
             if(cout_test_case)
-                cout<<p<<k;
+                cout<<p<<" "<<k<<" ";
             set<int> plants;
             for(int rj=0;rj<k;++rj) {
                 int plant_index;
                 cin>>plant_index;
                 if(cout_test_case)
-                    cout<<plant_index;
+                    cout<<plant_index<<" ";
                 plants.insert(plant_index);
-                plant_after[plant_index]=max(plant_after[plant_index],p);
+                catnip_lower_bound[plant_index]=max(catnip_lower_bound[plant_index], p);
             }
             pot_should_plant[p].push_back(plants);
             if(cout_test_case)
                 cout<<endl;
         }
+//        对关键盆计算应种草集合
         vector<int> after_count(m+1,0);
-        for(auto pot_index:plant_after)
-            after_count[pot_index]++;
-        //每盆艹可能被多个猫喜欢，检查多个猫中最大的盆，这个艹需要种在大于等于这个盆的位置
         bool can_do=true;
-        for(int pot_index=1; pot_index <= m; ++pot_index)
-            if(after_count[pot_index] >m -pot_index+1){
-                can_do=false;
-                break;
-            }
-        if(!can_do) {
-            cout << "no" << endl;
-            continue;
-        }
         //如果一个盆需要多个猫停在这，那需要种一颗对这些猫都吸引的艹
+//        对关键盆计算应种草集合
         vector<bool> catnip_planted(m+1,false);
-        vector<bool> pot_planted(m+1,false);
-        vector<set<int>> pot_possible_cn(m+1);
-        for(int pi=1;pi<=m;++pi){
-            if(pot_should_plant[pi].empty())continue;
-            auto set1=pot_should_plant[pi].front();
-            for(auto ss:pot_should_plant[pi]) {
-                std::set<int> temp_intersection; // 临时变量存储本次交集结果
-                std::set_intersection(
-                        set1.begin(), set1.end(),
-                        ss.begin(), ss.end(),
-                        std::inserter(temp_intersection, temp_intersection.begin())
-                );
-                set1 = std::move(temp_intersection); // 将本次交集结果赋给 set1，用于下次计算
-                if (set1.empty()) { // 提前终止，交集已为空
-                    break;
-                }
-            }
-            int not_planted=set1.size();
-            for(auto cn:set1){
-                if(catnip_planted[cn])
-                    --not_planted;
-            }
-            if (not_planted == 1) {
-                catnip_planted[*set1.begin()] = true;
-                pot_planted[pi] = true;
-            }
-            else
-                pot_possible_cn[pi] = set1;
-            if (set1.empty()||not_planted==0) {
-                can_do = false;
-                break;
-            }
-            if (set1.size() == 1) {
-                catnip_planted[*set1.begin()] = true;
-                pot_planted[pi] = true;
-            }
-            bool all_plant_should_plant_after_this_pot=true;
-            for(auto cn:set1){
-                if(plant_after[cn]<=pi){all_plant_should_plant_after_this_pot=false;
-                    break;}
-            }
-            if(all_plant_should_plant_after_this_pot){
-                can_do = false;
-                break;
-            }
-        }
-        if(!can_do) {
-            cout << "no" << endl;
-            continue;
-        }
-//        检查一颗艹有没有被重复种
-//尝试把没种的艹种了
+        for(auto pi :key_pots){
+            auto set1=set_intersect(pot_should_plant[pi]);
+//           过滤应种草集合，去除lb过高的
+            set<int> set2;
+            for(auto ci:set1)
+                if(catnip_lower_bound[ci]<=pi)
+                    set2.insert(ci);
 
-        vector<pair<int,int>> plant_after_sort;
-        for(auto pi:plant_after)
-            plant_after_sort.push_back(make_pair(pi,plant_after_sort.size()+1));
-        auto cmp=[&](const pair<int, int> &a, const pair<int, int> &b) {
-            return a.first<b.first;
-        };
-        sort(plant_after_sort.begin(), plant_after_sort.end(),cmp);
-        int not_planted_count=0;
-        for(int pi=1;pi<=m;++pi){
-            if(!pot_planted[pi])++not_planted_count;
-            for(int pj=1;pj<=pi;++pj){
-                if(pot_planted[pj])continue;
-                auto lb= upper_bound(plant_after_sort.begin(),plant_after_sort.end(), make_pair(not_planted_count,0),cmp);
-                int availabe_catnip=lb-plant_after_sort.begin();
-                if(availabe_catnip<not_planted_count){
-                    can_do=false;
-                    goto fail;
-                }
+            if(set2.empty()){
+                can_do= false;
+                goto finish;
             }
         }
-        fail:
+//        对猫草按lb排序
+        sort(catnip_lower_bound.begin(),catnip_lower_bound.end());
+        for(int ii=1;ii<=m;++ii)
+            if(catnip_lower_bound[ii]>ii) {
+                can_do= false;
+                goto finish;
+            }
+finish:
         cout<<(can_do?"yes":"no")<<endl;
+//        cout<<"tc shit "<<tc<<" " <<(can_do?"yes":"no")<<endl;
     }
 }
