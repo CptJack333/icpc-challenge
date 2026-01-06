5b8d98c341bf6e8da36b8cd0874b5baa2dee0fa0
CptJack <leninchen03@gamil.com>
2026-01-06 08:22:29 +0800

    25 f

--- 2025/F-HerdingCats/main.cpp
+++ 2025/F-HerdingCats/main.cpp
@@ -5,23 +5,33 @@ int main(){
     int t,n,m,p,k;
     cin>>t;
     for(int tc=0;tc<t;++tc){
+        bool cout_test_case=false;
         cin>>n>>m;
+        if(cout_test_case)
+            cout<<"test case "<<tc<<endl<<n<<m<<endl;
         vector<int> plant_after(m+1,0);
         vector<vector<set<int>>> pot_should_plant(m+1);
         for(int ri=0;ri<n;++ri){
             cin>>p>>k;
+            if(cout_test_case)
+                cout<<p<<k;
             set<int> plants;
             for(int rj=0;rj<k;++rj) {
                 int plant_index;
                 cin>>plant_index;
+                if(cout_test_case)
+                    cout<<plant_index;
                 plants.insert(plant_index);
                 plant_after[plant_index]=max(plant_after[plant_index],p);
             }
             pot_should_plant[p].push_back(plants);
+            if(cout_test_case)
+                cout<<endl;
         }
         vector<int> after_count(m+1,0);
         for(auto pot_index:plant_after)
             after_count[pot_index]++;
+        //每盆艹可能被多个猫喜欢，检查多个猫中最大的盆，这个艹需要种在大于等于这个盆的位置
         bool can_do=true;
         for(int pot_index=1; pot_index <= m; ++pot_index)
             if(after_count[pot_index] >m -pot_index+1){
@@ -32,6 +42,10 @@ int main(){
             cout << "no" << endl;
             continue;
         }
+        //如果一个盆需要多个猫停在这，那需要种一颗对这些猫都吸引的艹
+        vector<bool> catnip_planted(m+1,false);
+        vector<bool> pot_planted(m+1,false);
+        vector<set<int>> pot_possible_cn(m+1);
         for(int pi=1;pi<=m;++pi){
             if(pot_should_plant[pi].empty())continue;
             auto set1=pot_should_plant[pi].front();
@@ -47,14 +61,35 @@ int main(){
                     break;
                 }
             }
-            if (set1.empty()) {
+            int not_planted=set1.size();
+            for(auto cn:set1){
+                if(catnip_planted[cn])
+                    --not_planted;
+            }
+            if (not_planted == 1) {
+                catnip_planted[*set1.begin()] = true;
+                pot_planted[pi] = true;
+            }
+            else
+                pot_possible_cn[pi] = set1;
+            if (set1.empty()||not_planted==0) {
                 can_do = false;
                 break;
+            }
+            if (set1.size() == 1) {
+                catnip_planted[*set1.begin()] = true;
+                pot_planted[pi] = true;
             }
         }
         if(!can_do) {
             cout << "no" << endl;
             continue;
+        }
+//        检查一颗艹有没有被重复种
+//尝试把没种的艹种了
+        for(int pi=1;pi<=m;++pi){
+            if(pot_planted[pi])continue;
+
         }
         cout<<"yes"<<endl;
     }
