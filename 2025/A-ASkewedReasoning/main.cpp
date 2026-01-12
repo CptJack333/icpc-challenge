dfa17c87df1debf26d3c3d0af0338bd91808f398
CptJack <leninchen03@gamil.com>
2026-01-12 09:15:36 +0800

    25 a tmp

--- 2025/A-ASkewedReasoning/main.cpp
+++ 2025/A-ASkewedReasoning/main.cpp
@@ -20,5 +20,17 @@ int main(){
         auto [l,r]=child[root];
         return tree_size[root]=1+calc_tree_size(l)+calc_tree_size(r);
     };
+    int root=1;// 题目条件，ℓi and ri (i < ℓi ≤ n or ℓi = 0; i < ri ≤ n or ri = 0)，说明root是1
+    int min_spine=0x7FFFFFFF;
+//    从左边的脊梁骨里面选出一个最小/大的节点，并且不能有右子树
+    while(child[root].first || child[root].second){
+        if(child[root].second && !child[root].first){
+            cout<<"impossible"<<endl;
+            return 0;
+        }
+        if(!child[root].second)
+            min_spine=min(root,min_spine);
+        root=child[root].first;
 
+    }
 }
