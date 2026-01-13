c140455476a533c975bf5b20889e1b1ffbb541d4
CptJack <leninchen03@gamil.com>
2026-01-13 17:54:20 +0800

    25 a

--- 2025/A-ASkewedReasoning/main.cpp
+++ 2025/A-ASkewedReasoning/main.cpp
@@ -6,12 +6,12 @@ int main(){
     int n;
     cin>>n;
     vector<int> tree_size(n,0);
-    vector<pair<int,int>> child(n);
-    vector<int> parent(n,0);
+    vector<pair<int,int>> child(n+1);
+    vector<int> parent(n+1,0);
     for(int i=1;i<=n;++i){
         int l,r;
         cin>>l>>r;
-        child[n]= make_pair(l,r);
+        child[i]= make_pair(l,r);
         parent[l]=i;
         parent[r]=i;
     }
@@ -31,21 +31,28 @@ int main(){
         return 0;
     }
 
-    int root=1;// 题目条件，ℓi and ri (i < ℓi ≤ n or ℓi = 0; i < ri ≤ n or ri = 0)，说明root是1
-    int cur=root;
     bool find_min=true;
     vector<int> ret;
     for(auto rep=0;rep<2;++rep){
+        int root=1;// 题目条件，ℓi and ri (i < ℓi ≤ n or ℓi = 0; i < ri ≤ n or ri = 0)，说明root是1
         auto child_copy=child;
         auto parent_copy=parent;
         while (root) {
-//        root可拔除，且仅有root可拔除
+//        root可拔除，还得看看是不是只剩最后两个了
             if (!child[root].second) {
-//            auto tmp=root;
+                if(child[root].first&&!child[child[root].first].first&&find_min){
+                    auto leaf=child[root].first;
+                    parent[leaf]=0;
+                    child[root].first=0;
+                    ret.push_back(leaf);
+                    continue;
+                }
                 ret.push_back(root);
                 parent[child[root].first] = 0;
-                child[root].first = 0;
-                root = child[root].first;
+                auto lch=child[root].first;
+                child[root]={0,0};
+                root = lch;
+                continue;
             }
             auto p = root;
             //找到left spine上第一个右子树为空 的节点，这个节点可以拔出
@@ -53,20 +60,27 @@ int main(){
                 p = child[p].first;
             }
 //        找到了一个叶子结点的父节点，叶子节点也可以拔除,在找最小序列的时候拔叶子
+            auto take_out=[&](int p){
+                auto par=parent[p];
+                child[par].first=child[p].first;
+                parent[child[p].first]=par;
+                child[p]={0,0};
+                parent[p]=0;
+                p=par;
+                while(p){
+                    auto &ch = child[p];
+                    swap(ch.first, ch.second);
+                    auto par = parent[p];
+                    p=par;
+                }
+            };
             if (find_min && child[p].first && !child[child[p].first].first) {
                 auto leaf = child[p].first;
-                parent[leaf] = 0;
-                child[p].first = child[p].second;
-                child[p].second = 0;
+                take_out(leaf);
                 ret.push_back(leaf);
             } else {
 //            非叶子或者，最大序列的时候，拔枝干上的节点
-                auto pparent = parent[p];
-                auto lchild = child[p].first;
-                parent[lchild] = pparent;
-                child[pparent].first = lchild;
-                parent[p] = 0;
-                child[p].first = 0;
+                take_out(p);
                 ret.push_back(p);
             }
         }
@@ -76,6 +90,7 @@ int main(){
         find_min=!find_min;
         child=child_copy;
         parent=parent_copy;
+        ret.clear();
     }
     return 0;
 }
