aeee65576e3fa1bdf6e657706fd870646bba9930
CptJack <leninchen03@gamil.com>
2026-02-07 12:14:55 +0800

    25 d

--- 2025/D-BuggyRover/main.cpp
+++ 2025/D-BuggyRover/main.cpp
@@ -6,12 +6,12 @@ int main(){
     int r,c;
     cin>>r>>c;
     vector<vector<char>> g(r,vector<char>(c));
-    int si,sj;
+    int sr,sc;
     for(int i=0;i<r;++i)
         for(int j=0;j<c;++j){
             cin>>g[i][j];
             if(g[i][j]=='S')
-                si=i,sj=j;
+                sr=i,sc=j;
         }
     string path;
     cin>>path;
@@ -21,15 +21,15 @@ int main(){
         orders.push_back(order);
     }while(next_permutation(order.begin(),order.end()));
     int min_hit_times=0x7FFFFFFF;
-    int ii=si , jj=sj;
+    int rr=sr , cc=sc;
     for(auto order:orders){
         int hit_times=0;
         for(auto s:path){
             // auto matched=false;
             for(auto o:order){
-                int ni=ii+(o=='E'?1:(o=='W'?-1:0)), nj=jj+(o=='S'?1:(o=='N'?-1:0));
-                if(ni>=c ||ni<0 || nj>=r || nj<0)continue;
-                if(g[ni][nj]=='.'){
+                int nr=rr+(o=='S'?1:(o=='N'?-1:0)), nc=cc+(o=='E'?1:(o=='W'?-1:0));
+                if(nr>=r ||nr<0 || nc>=c || nc<0)continue;
+                if(g[nr][nc]=='.'){
                     if(o!=s)
                         ++hit_times;
                     // matched=true;
