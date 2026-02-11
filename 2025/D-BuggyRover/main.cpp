8596ca1ca8374d758857d3218731c796dca964b1
CptJack <leninchen03@gamil.com>
2026-02-11 08:43:56 +0800

    25 d

--- 2025/D-BuggyRover/main.cpp
+++ 2025/D-BuggyRover/main.cpp
@@ -15,35 +15,37 @@ int main(){
         }
     string path;
     cin>>path;
+
     string first_order="ENSW";
     vector<string> orders;
     do{
         orders.push_back(first_order);
     }while(next_permutation(first_order.begin(), first_order.end()));
+
     int hit_times=0;
     vector<bool> valid_orders(24,true);
-    int pr=sr , pc=sc;
+    int current_r=sr , current_c=sc;
     for(auto p:path){
-//        if (order=="NESW")
-//            asm("int $0x3");
-        int npr=pr+(p=='S'?1:(p=='N'?-1:0));
-        int npc=pc+(p=='E'?1:(p=='W'?-1:0));
+        int next_r= current_r + (p == 'S' ? 1 : (p == 'N' ? -1 : 0));
+        int next_c= current_c + (p == 'E' ? 1 : (p == 'W' ? -1 : 0));
+
         auto filter_orders=[&](){
             for (int oi = 0; oi < 24; ++oi) {
                 auto order = orders[oi];
                 // auto matched=false;
                 for (auto o: order) {
-                    int nr = pr + (o == 'S' ? 1 : (o == 'N' ? -1 : 0));
-                    int nc = pc + (o == 'E' ? 1 : (o == 'W' ? -1 : 0));
-                    if (nr >= r || nr < 0 || nc >= c || nc < 0)continue;
-                    if (g[nr][nc] == '#')continue;
-                    if (npr != nr || npc != nc) {
+                    int order_r = current_r + (o == 'S' ? 1 : (o == 'N' ? -1 : 0));
+                    int order_c = current_c + (o == 'E' ? 1 : (o == 'W' ? -1 : 0));
+                    if (order_r >= r || order_r < 0 || order_c >= c || order_c < 0)continue;
+                    if (g[order_r][order_c] == '#')continue;
+                    if (next_r != order_r || next_c != order_c) {
                         valid_orders[oi] = false;
                     }
                     break;
                 }
             }
         };
+
         filter_orders();
         bool no_valid_order=true;
         for(auto b:valid_orders)
@@ -56,9 +58,11 @@ int main(){
             valid_orders=vector<bool>(24,true);
             filter_orders();
         }
-        pr=npr;
-        pc=npc;
+
+        current_r=next_r;
+        current_c=next_c;
     }
+
     cout<<hit_times<<endl;
     return 0;
 }
