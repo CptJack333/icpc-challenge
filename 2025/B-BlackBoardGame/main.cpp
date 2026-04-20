112ac4ab65e3d642c376835fb2078b9057ad431f
CptJack <leninchen03@gamil.com>
2026-04-20 20:58:19 +0800

    25 B

--- 2025/B-BlackBoardGame/main.cpp
+++ 2025/B-BlackBoardGame/main.cpp
@@ -17,6 +17,47 @@ vector<bool> gen_prime(int n){
     return isPrime;
 }
 
+// round: 0 first 1 second
+bool dfs(int num, int n,int round, const vector<bool>& isPrime, vector<bool>& used){
+    vector<int> available_move;
+    for(int p=2;p*num<=n;++p){
+        if(isPrime[p] && !used[p*num])
+            available_move.push_back(p*num);
+    }
+    for(int p=2;p<num;++p)
+        if(isPrime[p] && num%p==0 && !used[num/p])
+            available_move.push_back(num/p);
+
+    if(available_move.empty())
+        return round==0;
+
+    if(round==0){
+        bool all_opponent_move_can_be_handled = true;
+        for (auto m: available_move) {
+            used[m] = true;
+            auto b = dfs(m, n, (round + 1) % 2, isPrime, used);
+            if (!b) {
+                all_opponent_move_can_be_handled=false;
+            }
+            used[m] = false;
+
+            if(!all_opponent_move_can_be_handled)
+                break;
+        }
+        return all_opponent_move_can_be_handled;
+    }else{
+//        bool opponent_move_has_solution=false;
+        for (auto m: available_move) {
+            used[m] = true;
+            auto b = dfs(m, n, (round + 1) % 2, isPrime, used);
+            used[m] = false;
+            if(b)
+                return true;
+        }
+        return false;
+    }
+}
+
 int main(){
     int t;
     cin>>t;
@@ -25,5 +66,17 @@ int main(){
         cin>>n;
         auto isPrime= gen_prime(n);
 
+        bool first_win=false;
+        for(auto strategy=2;strategy<=n;strategy+=2){
+            vector<bool> used(n+1,false);
+            used[strategy]=true;
+            if(dfs(strategy,n,0,isPrime,used)) {
+                cout << "first " << strategy << endl;
+                first_win=true;
+                break;
+            }
+        }
+        if(!first_win)
+            cout<<"second"<<endl;
     }
 }
