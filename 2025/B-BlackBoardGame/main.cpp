ce514223c6ef5f7fbb2fea8463fc40a7e036b036
CptJack <leninchen03@gamil.com>
2026-05-06 16:51:11 +0800

    25 B 匈牙利算法找最大匹配

--- 2025/B-BlackBoardGame/main.cpp
+++ 2025/B-BlackBoardGame/main.cpp
@@ -31,41 +31,38 @@ vector<bool> gen_prime(int n){
 }
 
 // round: 0 first 1 second
-bool dfs(int num, int n,int round, const vector<bool>& isPrime, vector<bool>& used){
-    vector<int> available_move=graph[num];
 
-    if(available_move.empty())
-        return round==0;
+int n;
+vector<int> match_to;
+vector<bool> visited;
 
-    if(round==0){
-        bool all_opponent_move_can_be_handled = true;
-        for (auto m: available_move) {
-            if(used[m])
-                continue;
-            used[m] = true;
-            auto b = dfs(m, n, (round + 1) % 2, isPrime, used);
-            if (!b) {
-                all_opponent_move_can_be_handled=false;
-            }
-            used[m] = false;
-
-            if(!all_opponent_move_can_be_handled)
-                break;
-        }
-        return all_opponent_move_can_be_handled;
-    }else{
-//        bool opponent_move_has_solution=false;
-        for (auto m: available_move) {
-            if(used[m])
-                continue;
-            used[m] = true;
-            auto b = dfs(m, n, (round + 1) % 2, isPrime, used);
-            used[m] = false;
-            if(b)
+// 匈牙利算法 DFS 实现：尝试给 u 找匹配
+bool dfs(int u) {
+    // 遍历 u 的所有邻接点
+    for (int v : graph[u]) {
+        if (!visited[v]) {
+            visited[v] = true;
+            // 如果 v 未匹配，或匹配它的节点能找到新的匹配。 如果 v 没人要，或者 占有 v 的人能找到别人 → 我要了
+            if (match_to[v] == 0 || dfs(match_to[v])) {
+                match_to[v] = u;  // 更新匹配关系
                 return true;
+            }
         }
-        return false;
     }
+    return false;
+}
+
+//匈牙利算法返回最大匹配的数目
+int hungarianAssignment(){
+    match_to.clear();
+    match_to.resize(n+1, 0);
+    int res = 0;
+    for (int i = 1; i <= n; ++i) {
+        visited.clear();
+        visited.resize(n+1, false);
+        if (dfs(i)) res++;
+    }
+    return res;
 }
 
 int main(){
@@ -73,26 +70,37 @@ int main(){
     cin>>t;
 //    t=1;
     for(int case_i=0;case_i<t;++case_i){
-        int n;
         cin>>n;
 //        n=138;
 
-        graph.clear();
-        graph.resize(n+1);
         auto isPrime= gen_prime(n);
 
         if(n<118){
-            bool first_win = false;
-            for (auto strategy = 2; strategy <= n; strategy += 2) {
-                vector<bool> used(n + 1, false);
-                used[strategy] = true;
-                if (dfs(strategy, n, 0, isPrime, used)) {
-                    cout << "first " << strategy << endl;
-                    first_win = true;
-                    break;
-                }
+            graph.clear();
+            graph.resize(n+1);
+            for (int x = 1; x <= n; x++) {
+                for (int y = 1; y <  x; y++) if (x%y == 0 && isPrime[x/y])
+                    graph[x].push_back(y);
+                for (int y = x; y <= n; y++) if (y%x == 0 && isPrime[y/x])
+                    graph[x].push_back(y);
             }
-            if (!first_win)
+
+            bool first_win = false;
+            auto strategy = 2;
+            for (; strategy <= n; strategy += 2) {
+                int cnt = 0;
+                auto match_num=hungarianAssignment();
+
+                auto tmp=graph[strategy];
+                graph[strategy].clear();
+                auto match_num2=hungarianAssignment();
+                graph[strategy]=tmp;
+
+                if (match_num2 == match_num) break;
+            }
+            if (strategy<=n)
+                cout<<"first "<<strategy;
+            else
                 cout << "second" << endl;
         }else{
             vector<int> pri;
